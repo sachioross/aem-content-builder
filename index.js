a39e938512645034cbcb7d7156ffefd22ba52406
Douglas Ross <ross@adobe.com>
2016-10-22 00:37:37 -0400

    Adding a define function to create new components
    This is basically an abstraction of the prototyping aspect of the framework in order to minimize knowledge required by implementors / reduce chance of error.

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ var PageFactory = require("./structure");
 var Component = require("./Component.js");
 var Page = require("./PageComponent.js");
 
+function define(cmp, cmpDef) {
+  cmp.prototype = Object.create(Component.prototype, cmpDef);
+  return cmp;
+}
+
+function definePage(cmp, cmpDef) {
+  cmp.prototype = Object.create(Page.prototype, cmpDef);
+  return cmp;
+}
+
 module.exports = {
   componentFactory : ComponentFactory,
   pageFactory : PageFactory,
   Component : Component,
-  Page : Page
+  Page : Page,
+  Define : define,
+  DefinePage : definePage
 }
