8956c6fff35f92e8b76c7eeb3e612b6d0bf49725
Douglas Ross <sachioross@gmail.com>
2020-06-10 16:25:59 -0700

    Created a simple create-page example

--- examples/wknd/create-page.js
+++ examples/wknd/create-page.js
@@ -8,16 +8,19 @@ class ContentPage extends aem.components.core.Page {
         this.props["jcr:content/sling:resourceType"] = "wknd/components/structure/page";
         this.setContent("jcr:title", title ? title : "TITLE MISSING"); 
     }
-
 }
 
 // Instantiate a new page titled, "Migrated Page". This will be an empty page for now.
 let cp = new ContentPage("Auto-Generated Page");
 
+// Set a multi-value property
+cp.setMultiValueProp("jcr:content/migratedTags", ["one", "two", 3]);
+
 // Instantiate a new AEM request with the proper target location
 const target = "http://localhost:4502"
 let req = new aem.request.POST(`${target}/content/wknd/us/en/auto-generated`); 
 
+console.log(cp.getData());
 // Add the appropriate credentials and payload
 req.credentials("admin", "admin").payload(cp.getData());
 
