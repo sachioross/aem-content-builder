67c92e136339ae511dffc900af5541fe1735e97a
stevepelhan <spelhan@adobe.com>
2020-06-11 14:39:20 -0400

    Added accordian.test.js file

---
+++ test/accordian.test.js
@@ -0,0 +1,21 @@
+const aem = require('../index');
+
+test('Component Functions - Happy Path', () => {
+    let testContent = {
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"core/wcm/components/accordion/v1/accordian", 
+        "id":"abc", 
+        "expandedItems":"header",
+        "singleExpansion":false,
+        "headingElement": "h3"
+    }
+
+    let accordian = new aem.components.core.Accordian();
+
+    accordian.setExpandedItems"header");
+    accordian.setSingleExpansion(false);
+    accordian.setHeadingElement("h3");
+    accordian.setID("abc");
+
+    expect(accordian.getData()).toStrictEqual(testContent);
+});
