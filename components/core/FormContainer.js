f6840489ea3b21b51d8fe3284dcc4743eb049716
joey-kubicki <67123408+joey-kubicki@users.noreply.github.com>
2020-06-19 15:09:28 -0400

    Add files via upload

---
+++ components/core/FormContainer.js
@@ -0,0 +1,35 @@
+const AbstractComponent = require('../AbstractComponent');
+
+/**
+ * The Form Buttom Core Component. In addition to the Component methods, this component provides several 
+ * convience methods to set known properties. 
+ * 
+ * Based on the documentation found at:
+ * https://github.com/adobe/aem-core-wcm-components/tree/master/content/src/content/jcr_root/apps/core/wcm/components/form/container/v2/container
+ */
+
+ class FormContainer extends AbstractComponent {
+
+ 	constructor() {
+ 		super();
+ 		this.props["sling:resourceType"] = "core/wmc/components/form/container/v2/container"
+ 	}
+
+ 	setActionType(type) {
+ 		this.props["actionType"] = type;
+ 	}
+
+ 	setWorkflowModel(model) {
+ 		this.props["workflowModel"] = model;
+ 	}
+
+ 	setRedirect(redirect) {
+ 		this.props["redirect"] = redirect;
+ 	}
+
+ 	setID(id) {
+ 		this.props["id"] = id;
+ 	}
+ }
+
+ module.exports = FormContainer;
---
+++ components/core/LanguageNavigation.js
@@ -0,0 +1,31 @@
+const AbstractComponent = require('../AbstractComponent');
+
+/**
+ * The Language Navigation Core Component. In addition to the Component methods, this component provides several 
+ * convience methods to set known properties. 
+ * 
+ * Based on the documentation found at:
+ * https://github.com/adobe/aem-core-wcm-components/tree/master/content/src/content/jcr_root/apps/core/wcm/components/languagenavigation/v1/languagenavigation
+ */
+
+ class LanguageNavigation extends AbstractComponent {
+
+ 	constructor() {
+ 		super();
+ 		this.props["sling:resourceType"] = "core/wmc/components/languagenavigation/v1/languagenavigation"
+ 	}
+
+ 	setNavigationRoot(nav_root) {
+ 		this.props["navigationRoot"] = nav_root;
+ 	}
+
+ 	setStructureDepth(depth) {
+ 		this.props["structureDepth"] = depth;
+ 	}
+
+ 	setID(id) {
+ 		this.props["id"] = id;
+ 	}
+}
+
+module.exports = LanguageNavigation;
