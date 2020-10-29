ba58ebd5cb8b8c6eb53e949f0a6afb3a30b7cf74
Douglas Ross <sachioross@gmail.com>
2020-10-29 12:05:27 -0700

    Updating to use strings in params

--- request/Handler.js
+++ request/Handler.js
@@ -61,15 +61,15 @@ function login(host, user, pass, opts) {
     })
 }
 
-function deletePage(path, loginCookie, opts) {
+function deletePage(host, path, loginCookie, opts) {
 
     opts = opts ? opts : {};
  
     let deleteRequest = new POST(`${host}/bin/wcmcommand`);
     deleteRequest.payload({
         _charset_: "UTF-8",
-        force : true, 
-        checkChildren : false, 
+        force : "true", 
+        checkChildren : "false", 
         cmd: "deletePage", 
         path: path
     }).setCookie(loginCookie);
